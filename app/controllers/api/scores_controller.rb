53a0f801858d2d63006cb3592249ab2e8c76ed13
Haymarn <haymarn@nexlabs.co>
2017-04-14 09:23:06 +0630

    init bowlingame project

---
+++ app/controllers/api/scores_controller.rb
@@ -0,0 +1,8 @@
+class Api::ScoresController  < ApplicationController
+	skip_before_filter :verify_authenticity_token
+
+	def create
+		
+	end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  
+  namespace :api do
+    resources :scores, only: [:create]
+  end
+
+end
